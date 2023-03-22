057daca2e5544a621ec3341dc66414b6ec0eb27d
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-03-22 14:17:50 +0300

    lesson 1

---
+++ src/P0_Comparible/T1_sort.java
@@ -0,0 +1,19 @@
+package P0_Comparible;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class T1_sort {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Zaur");
+        list.add("Maria");
+        list.add("Ivan");
+
+        System.out.println("Before sort - " + list);
+        Collections.sort(list);
+        System.out.println("After sort - " + list);
+
+    }
+}
---
+++ src/P2_Collections/L1_List/T1.java
@@ -0,0 +1,7 @@
+package P2_Collections.L1_List;
+
+public class T1 {
+    public static void main(String[] args) {
+
+    }
+}
